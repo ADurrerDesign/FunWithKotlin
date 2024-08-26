eb1682f22934aed3108c2db22449bf80355209d6
ADurrerDesign <ADurrerDesign@gmail.com>
2024-08-26 14:27:42 -0600

    redid the nameformatter challenge

--- NameFormatterPractice/src/main.kt
+++ NameFormatterPractice/src/main.kt
@@ -9,16 +9,32 @@ Write a Name Formatter function
     - printFormattedName should print the result of calling the format function
  */
 
-fun printFormattedName(name: String) {
-    println(name)
+/*Notes: concepts to remember function parameters. One can input function as an input
+        Example fun primaryName(p1,1p2, functionalPara (Type, Type) -> returnTye { function of primaryFunc}
+*/
+
+fun printFormattedName(firstName:String, lastName:String, format: (String, String) -> String) {
+    println(format (firstName, lastName))
 }
 
-fun formatName (firstName: String,lastName: String): String {return firstName + " " + lastName}
+/* to input the val into the functional parameter,
+    Format the val to match the functional para input and output
+    then use lamda to create format
+ */
+val basicFormat: (String, String) -> String = {firstName, lastName -> "$firstName $lastName"}
+val introduceMyselfFormat: (String, String) -> String = {firstName, lastName -> "Hello my name is $firstName $lastName";
+
+
+}
 
 fun main() {
-    printFormattedName(formatName("John","Doe"))
-    printFormattedName(formatName("X", "86"))
-    printFormattedName(formatName("Hello", "World"))
+    printFormattedName("Andy", "Durrer", basicFormat)
+    printFormattedName("Andy", "Durrer", introduceMyselfFormat)
+    //inject a ladma function parameter instead of using a val.
+    printFormattedName("Testing", "Lamda") { firstName, lastName ->
+        "I am $firstName $lastName to replace the function parameter"
+    }
+
 }
 
 
